c3277365099e79c994a9328443d9ec6d7f33b49a
leandrodrc21 <51094309+leandrodrc21@users.noreply.github.com>
2020-06-23 12:16:37 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,31 @@
 <?php
 // Connection variables
-$dbhost	= "mysqld";	   // localhost or IP
+/*$dbhost	= "mysqld";	   // localhost or IP
 $dbuser	= "php";		  // database username
 $dbpass	= "12345678";		     // database password
-$dbname	= "miphp";    // database name
+$dbname	= "miphp";    // database name*/
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db="problem";
+
+$con=mysqli_connect($host,$user,$pass,$db);
+if(!$con)
+{
+	print("Not Connected<br>".mysql_error());
+}
+else
+{
+	echo("Connected");
+}
+
+
+
+
+
+
+
+
 ?>
